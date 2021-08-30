5c3a27fa23de15debe2fbe601625463e1555da7f
Ronald A. Richardson <me@ron.dev>
2021-08-30 14:31:21 +0800

    patch stripHtml util

--- src/utils/strip-html.js
+++ src/utils/strip-html.js
@@ -1,3 +1,9 @@
-const stripHtml = (html) => html.replace(/<[^>]*>?/gm, '');
+const stripHtml = (html = '') => {
+    if (typeof html === 'string') {
+        return html.replace(/<[^>]*>?/gm, '');
+    }
+    
+    return html;
+};
 
 export default stripHtml;
