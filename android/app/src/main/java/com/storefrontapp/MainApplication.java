ad5b8887e4e30562a663d93ae65d21ec48e45562
Ronald A. Richardson <me@ron.dev>
2022-05-30 14:17:23 +0800

    additional settings for config setup on android

--- android/app/src/main/java/com/storefrontapp/MainApplication.java
+++ android/app/src/main/java/com/storefrontapp/MainApplication.java
@@ -1,9 +1,11 @@
 package com.storefrontapp;
 
+// import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          // packages.add(new ReactNativeConfigPackage());
           return packages;
         }
 
