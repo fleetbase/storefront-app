183102b3e682bf8b63d12cf9cceb9c98e29b4132
Turtuvshin <toroo.byamba@gmail.com>
2024-03-05 17:53:44 +0800

    Fix saved place header

--- src/features/Account/screens/SavedPlacesScreen.js
+++ src/features/Account/screens/SavedPlacesScreen.js
@@ -66,7 +66,7 @@ const SavedPlacesScreen = ({ navigation, route }) => {
     }, []);
 
     return (
-        <View style={[tailwind('w-full h-full bg-white')]}>
+        <View style={[tailwind('w-full h-full bg-white'), { paddingTop: insets.top }]}>
             <FlatList
                 data={places}
                 onRefresh={loadPlaces}
