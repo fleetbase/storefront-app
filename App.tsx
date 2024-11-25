1f4011eeb6ecbaad533453987930e4c8b9ba2a8d
Ronald A. Richardson <me@ron.dev>
2024-11-25 21:07:32 +0800

    latest

--- App.tsx
+++ App.tsx
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { PropsWithChildren } from 'react';
 import { useColorScheme } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { TamaguiProvider, Theme } from 'tamagui';
+import { Toasts } from '@backpackapp-io/react-native-toast';
 import { PortalProvider, PortalHost } from '@gorhom/portal';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useTheme from './src/hooks/use-theme';
 import AppNavigator from './src/navigation/AppNavigator';
 import config from './tamagui.config';
 
 function App(): React.JSX.Element {
     const { theme } = useTheme();
-    console.log('App');
 
     return (
         <TamaguiProvider config={config}>
             <PortalProvider>
                 <Theme name={theme}>
                     <GestureHandlerRootView style={{ flex: 1 }}>
-                        <AppNavigator />
+                        <PortalProvider>
+                            <SafeAreaProvider>
+                                <AppNavigator />
+                                <Toasts extraInsets={{ bottom: 80 }} />
+                                <PortalHost name='MainPortal' />
+                                <PortalHost name='BottomSheetPanelPortal' />
+                                <PortalHost name='LocationPickerPortal' />
+                            </SafeAreaProvider>
+                        </PortalProvider>
                     </GestureHandlerRootView>
                 </Theme>
-                <PortalHost name='MainPortal' />
-                <PortalHost name='LocationPickerPortal' />
             </PortalProvider>
         </TamaguiProvider>
     );
