8243a3ef6c6b2d135d8a0a0033f555c422d8b4d3
Ronald A. Richardson <me@ron.dev>
2025-02-20 15:46:00 +0800

    few fixes on android

--- index.native.tsx
+++ index.native.tsx
@@ -2,9 +2,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import { enableScreens } from 'react-native-screens';
 import 'react-native-get-random-values';
 import 'react-native-gesture-handler';
 
-enableScreens();
 AppRegistry.registerComponent(appName, () => App);
